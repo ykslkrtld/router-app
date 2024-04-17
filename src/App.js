01fa9541a862786ecc0cdd9500fccc81423b2be6
yksl <yukselkurtuldu9@gmail.com>
2024-04-17 11:51:40 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Login from './pages/Login';
-import NavBar from './pages/NavBar';
-import Home from "./pages/Home"
-import About from "./pages/About"
-import { useState } from 'react';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import NavBar from "./pages/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { useState } from "react";
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false)
- 
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
-    <> 
-    
-    {!isLogged ? <Login isLogged={isLogged} setIsLogged={setIsLogged} /> : 
-    <BrowserRouter>
-    <NavBar isLogged={isLogged} setIsLogged={setIsLogged}/>
-    <Routes>
-      <Route path='/home' element={<Home/>} />
-      <Route path='/about' element={<About/>}/>
-    </Routes>
- 
-    </BrowserRouter>
-    }
+    <>
+      {!isLogged ? (
+        <Login isLogged={isLogged} setIsLogged={setIsLogged} />
+      ) : (
+        <BrowserRouter>
+          <NavBar isLogged={isLogged} setIsLogged={setIsLogged} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -5,14 +5,14 @@ const NavBar = ({isLogged, setIsLogged}) => {
   return (
     <nav className="navbar navbar-expand bg-body-tertiary">
   <div className="container">
-    <a className="navbar-brand" href="/home">Yksl</a>
+    <a className="navbar-brand" href="/">Yksl</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/about">About</Link>
